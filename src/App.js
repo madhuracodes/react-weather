fcd69a9f312689274e2823b41f03092327537180
madhuracodes <madhura84@gmail.com>
2025-03-20 11:07:35 +0100

    added spinner-loaded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import Weather from "./Weather";
+import { Audio } from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>Hello World </h1>
         <Weather />
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+        />
       </header>
     </div>
   );
