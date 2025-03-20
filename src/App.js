d883e769a556aa02484be1a9692ae67ebfb304ee
madhuracodes <madhura84@gmail.com>
2025-03-20 11:28:56 +0100

    added a footer

--- src/App.js
+++ src/App.js
@@ -18,7 +18,17 @@ function App() {
           wrapperClass
         />
         <footer>
-          This project was coded by Madhura and is open-sourced on
+          This project was coded by Madhura and is open-sourced on{" "}
+          <a
+            href="https://github.com/madhuracodes/react-weather"
+            target="_blank"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://maddy-react-weather.netlify.app/" target="_blank">
+            Netlify
+          </a>{" "}
         </footer>
       </header>
     </div>
